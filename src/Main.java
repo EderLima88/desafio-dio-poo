1fc7f846dca3b40bd9e7bce6e433745e0affb12a
EderLima88 <eder.ebl@gmail.com>
2025-06-18 18:32:04 -0300

    Criado main, curso mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("um curso sobre Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso C");
+        curso2.setDescricao("um curso sobre C");
+        curso2.setCargaHoraria(9);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Aula");
+        mentoria1.setDescricao("Aula de java");
+        mentoria1.setData(LocalDate.now());
+
+
+
+        System.out.println(curso1);
+        System.out.println(curso1);
+        System.out.println(mentoria1);
+
     }
 }
